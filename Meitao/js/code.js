4b0e7111b77340ae3c0b72dc2a870bc53fef68e3
liduanping <2450408807@qq.com>
2016-08-19 12:15:10 +0800

    add

---
+++ Meitao/js/code.js
@@ -0,0 +1,49 @@
+﻿function mAlert(Msg, url) {
+    layer.closeAll();
+    if (url != null && url != 'undefined' && url.length > 0) {
+        layer.open({
+            content: Msg,
+            btn: ['确认'],
+            shadeClose: false,
+            yes: function () {
+                location.href = url;
+            }
+        });
+    }
+    else {
+        layer.open({ content: Msg, time: 5 });
+    }
+}
+
+function mAlertClose(Msg) {
+    layer.closeAll();
+    layer.open({
+        content: Msg,
+        btn: ['关闭'],
+        shadeClose: false,
+        yes: function () {
+            location.href = location.href;
+        }
+    });
+
+}
+
+
+function mError(Msg) {
+
+    layer.closeAll();
+    layer.open({
+        content: Msg,
+        style: 'background-color:#F05133; color:#fff;',
+        time: 5
+    });
+}
+function mLoading() {
+    layer.closeAll();
+    layer.open({ type: 2, shadeClose: false });
+}
+
+function mClose(){
+    layer.closeAll();
+}
+
